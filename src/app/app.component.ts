4ebe124192baa25ec382420c9b380916f7c6c045
Darlan de Castro Iribarrem <c82431a@serasa.intranet@wksrhllx27.serasa.intranet>
2020-01-16 16:02:06 -0300

    start

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+
+  title = 'translate';
+
+  constructor(public translate: TranslateService) {
+
+    translate.addLangs(['pt-br', 'en']);
+    translate.setDefaultLang('pt-br');
+
+    //const browserLang = translate.getBrowserLang();
+    //translate.use(browserLang.match(/pt|en/) ? browserLang : 'en');
+  }
+}
